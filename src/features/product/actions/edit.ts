e1365795b572a0c06bfc67cbcb2c12df09c355d8
Mohammad Sharifi Sadeghi <m.sharifisadeghi@khu.ac.ir>
2023-08-03 16:42:31 +0430

    edit product

---
+++ src/features/product/actions/edit.ts
@@ -0,0 +1,66 @@
+import Error from '../error';
+import { err, ok, Result } from 'never-catch';
+import { Product, ProductModel } from '../schema';
+import { Connection } from '../../../utils/connection';
+
+const edit = async (
+    connection: Connection,
+    { id, title, description, price }: ProductModel<['id'], ['title', 'description', 'price']>
+): Promise<Result<{ id: ProductModel['id'] }, Error>> => {
+    // validation
+    if (!ProductModel.id.Validate(id)) {
+        return err([204]);
+    }
+    if (title !== undefined && !ProductModel.title.Validate(title)) {
+        return err([201]);
+    }
+    if (description !== undefined && !ProductModel.description.Validate(description)) {
+        return err([202]);
+    }
+    if (price !== undefined && !ProductModel.price.Validate(price)) {
+        return err([203]);
+    }
+
+    if (title === undefined && description === undefined && price === undefined) {
+        return err([205]);
+    }
+
+    // check product existence
+    const getProductResult = await Product.select(
+        ['id', 'labID'] as const,
+        context => context.colCmp('id', '=', id)
+    ).exec(connection.client, ['get', 'one']);
+    if (!getProductResult.ok) {
+        return err(
+            getProductResult.error === false ? [302] : [401, getProductResult.error]
+        );
+    }
+
+    // permission
+    if (getProductResult.value.labID !== connection.user.id) {
+        return err([301]);
+    }
+
+    // edit
+    const editProductResult = await Product.update(
+        {
+            title,
+            description,
+            price
+        },
+        context => context.colCmp('id', '=', id),
+        ['id'] as const,
+        {
+            ignoreInSets: true
+        }
+    ).exec(connection.client, ['get', 'one']);
+    if (!editProductResult.ok) {
+        return err([401, editProductResult]);
+    }
+
+    return ok({
+        id: editProductResult.value.id
+    });
+};
+
+export default edit;
--- src/features/product/error.ts
+++ src/features/product/error.ts
@@ -2,7 +2,10 @@ type Error =
     | [201] /* invalid title */
     | [202] /* invalid description */
     | [203] /* invalid price */
+    | [204] /* invalid id */
+    | [205] /* at least one change is required */
     | [301] /* permission denied */
+    | [302] /* product does not exist */
     | [401, unknown]
 
 export default Error;
