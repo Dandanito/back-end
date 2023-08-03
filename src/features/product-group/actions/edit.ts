d70a6c59d3ff832f366c30f0b917548fd991ec76
Mohammad Sharifi Sadeghi <m.sharifisadeghi@khu.ac.ir>
2023-08-03 13:29:26 +0430

    product group edit

--- src/features/product-group/actions/edit.ts
+++ src/features/product-group/actions/edit.ts
@@ -4,11 +4,14 @@ import { err, ok, Result } from 'never-catch';
 import Error from '../error';
 import { Role } from '../../user/roles';
 
-const add = async (
+const edit = async (
     connection: Connection,
-    title: ProductGroupModel['title']
+    { id, title }: ProductGroupModel<['id', 'title']>
 ): Promise<Result<{ id: ProductGroupModel['id'] }, Error>> => {
     // validation
+    if (!ProductGroupModel.id.Validate(id)) {
+        return err([202]);
+    }
     if (!ProductGroupModel.title.Validate(title)) {
         return err([201]);
     }
@@ -18,18 +21,22 @@ const add = async (
         return err([301]);
     }
 
-    // add
-    const addProductGroupResult = await ProductGroup.insert(
-        [{ title }],
+    // edit product group
+    const editProductGroupResult = await ProductGroup.update(
+        {
+            title
+        },
+        context => context.colCmp('id', '=', id),
         ['id'] as const
     ).exec(connection.client, ['get', 'one']);
-    if (!addProductGroupResult.ok) {
-        return err([401, addProductGroupResult.error]);
+    if (!editProductGroupResult.ok) {
+        return err([401, editProductGroupResult.error]);
     }
 
     return ok({
-        id: addProductGroupResult.value.id
+        id: editProductGroupResult.value.id
+
     });
 };
 
-export default add;
+export default edit;
--- src/features/product-group/error.ts
+++ src/features/product-group/error.ts
@@ -1,5 +1,6 @@
 type Error =
     | [201] /* invalid title */
+    | [202] /* invalid id */
     | [301] /* permission denied */
     | [401, unknown]
 
