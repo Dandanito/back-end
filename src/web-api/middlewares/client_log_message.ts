9b2cc2fd967c90df357a9dc9b150a6e207ef325b
Mohammad Sharifi Sadeghi <m.sharifisadeghi@khu.ac.ir>
2023-08-10 14:32:06 +0430

    web-api middlewares

---
+++ src/web-api/middlewares/client_log_message.ts
@@ -0,0 +1,82 @@
+import { pool } from '../../db';
+import logError from '../utils/logError';
+import { Request, Response } from 'express';
+import { err, ok, Result } from 'never-catch';
+import { FEATURES } from '../../utils/features';
+import addLog from '../../features/log/actions/add';
+import { Connection } from '../../utils/connection';
+import evalClientData from '../utils/evalClientData';
+import { UserModel } from '../../features/user/schema';
+
+const client_log_message =
+    (
+        api: string,
+        action: (
+            req: Request,
+            res: Response,
+            client: Connection['client']
+        ) => Promise<
+            Result<
+                {
+                    feature: keyof typeof FEATURES | null;
+                    code: number;
+                    data?: unknown;
+                    userID: UserModel['id'];
+                },
+                {
+                    feature: keyof typeof FEATURES | null;
+                    code: number;
+                    data?: unknown;
+                }
+            >
+        >
+    ) =>
+    async (req: Request, res: Response) => {
+        const response = await pool
+            .transaction(async client => {
+                // action
+                const actionResult = await action(req, res, client);
+                if (!actionResult.ok) {
+                    return actionResult;
+                }
+                const response = actionResult.value;
+
+                const now = new Date();
+                // log
+                const addLogResult = await addLog(
+                    { client },
+                    {
+                        api,
+                        createdAt: now,
+                        headers: JSON.stringify(req.headers),
+                        body: JSON.stringify(req.body),
+                        response: JSON.stringify(response)
+                    }
+                );
+                if (!addLogResult.ok) {
+                    const [code, data] = addLogResult.error;
+                    return err({
+                        feature: FEATURES.Log,
+                        code,
+                        data
+                    });
+                }
+
+                return actionResult.ok ? ok(response) : err(response);
+            }, 'serializable')
+            .catch(e => err({ feature: null, code: 0, data: e }));
+
+        if (!response.ok) {
+            await logError({
+                headers: req.headers,
+                body: req.body,
+                response
+            });
+        }
+        const { feature, code, data } = response.ok
+            ? response.value
+            : response.error;
+        res.send({ feature, code, data: evalClientData(feature, code, data) });
+    };
+
+export default client_log_message;
---
+++ src/web-api/middlewares/client_verify_log_message.ts
@@ -0,0 +1,104 @@
+import { pool } from '../../db';
+import { Request, Response } from 'express';
+import logError from '../utils/logError';
+import { err, ok, Result } from 'never-catch';
+import { FEATURES } from '../../utils/features';
+import addLog from '../../features/log/actions/add';
+import { Connection } from '../../utils/connection';
+import evalClientData from '../utils/evalClientData';
+import verify from '../../features/token/actions/verify';
+import { TokenModel } from '../../features/token/schema';
+
+const client_verify_log_message =
+    (
+        api: string,
+        action: (
+            req: Request,
+            res: Response,
+            connection: Connection
+        ) => Promise<
+            Result<
+                {
+                    feature: keyof typeof FEATURES | null;
+                    code: number;
+                    data?: unknown;
+                },
+                {
+                    feature: keyof typeof FEATURES | null;
+                    code: number;
+                    data?: unknown;
+                }
+            >
+        >
+    ) =>
+    async (req: Request, res: Response) => {
+        const response = await pool
+            .transaction(async client => {
+                // verify
+                const secret = TokenModel.secret.Parse(req.headers.secret);
+                if (secret === undefined) {
+                    return err({
+                        feature: FEATURES.Token,
+                        code: 101,
+                        data: undefined
+                    });
+                }
+                const verifyResult = await verify({ client }, secret);
+                if (!verifyResult.ok) {
+                    const [code, data] = verifyResult.error;
+                    return err({
+                        feature: FEATURES.Token,
+                        code,
+                        data
+                    });
+                }
+
+                // action
+                const actionResult = await action(req, res, { client, user: { id: verifyResult.value.userID, role: verifyResult.value.role} });
+                if (!actionResult.ok) {
+                    return actionResult;
+                }
+                const response = actionResult.value;
+
+                const now = new Date();
+
+                const addLogResult = await addLog(
+                    { client },
+                    {
+                        api,
+                        createdAt: now,
+                        headers: JSON.stringify(req.headers),
+                        body: JSON.stringify(req.body),
+                        response: JSON.stringify(response)
+                    }
+                );
+                if (!addLogResult.ok) {
+                    const [code, data] = addLogResult.error;
+                    return err({
+                        feature: FEATURES.Log,
+                        code,
+                        data
+                    });
+                }
+
+                return actionResult.ok ? ok(response) : err(response);
+            }, 'serializable')
+            .catch(e => {
+                console.log(e);
+                return err({ feature: null, code: 0, data: e })
+            });
+
+        if (!response.ok) {
+            await logError({
+                headers: req.headers,
+                body: req.body,
+                response
+            });
+        }
+        const { feature, code, data } = response.ok
+            ? response.value
+            : response.error;
+        res.send({ feature, code, data: evalClientData(feature, code, data) });
+    };
+
+export default client_verify_log_message;
