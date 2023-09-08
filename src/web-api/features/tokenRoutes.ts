93eaa25c6ca0933483cc87b5996e40213f71f42e
Mohammad Sharifi Sadeghi <m.sharifisadeghi@khu.ac.ir>
2023-09-08 17:40:51 +0430

    adding swagger to tokenRoutes

--- src/web-api/features/tokenRoutes.ts
+++ src/web-api/features/tokenRoutes.ts
@@ -18,6 +18,37 @@ const WhoAmIRoute = '/whoAmI';
 const VerifyRoute = '/verify';
 
 const token = (app: Express) => {
+    /**
+     * @swagger
+     * /login:
+     *   post:
+     *     summary: Login with provided data.
+     *     description: Login with provided data.
+     *     tags:
+     *      - Token
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               phoneNumber:
+     *                 type: string
+     *               emailAddress:
+     *                 type: string
+     *               password:
+     *                 type: string
+     *               captcha:
+     *                 type: string
+     *             example:
+     *               username: "testUserName"
+     *               password: "12345678"
+     *               captcha: "03ADUVZwAHn1K5Z0p8QP7_IjBN_C9UOI15XQicLKUOjZpa1QTqpDiojxE8w9oC7C6TiZw6gA-gu3RjmKuLGOajA77ZwixcV3YFyiyw_0bIz8XR2cqSEs2SQGGngsUaTAwb44XsIcfePzZmIdwzwLlvDQlg5WXpbb0Id-AoQ9Dd_fNHcbIpe4L_8_70ONCWW0ZyZxhZ9lY2g5KLdyiyZ7u7MbZRFE8gnBMhH2MsteOu6tkOrl8chWMdJ2_Rn5b4p4MTw7Q5WC82GdoN_eGWK-QrjLiWbPVYuz_PSFsRqHq2VkXx4PIR6jxuy2yN0k1Qhe1xSXKHVFGcBr6byS_8JtsoTDVszSq3PKjk6LzYzoSkryOpbdpteYHV2kMBAeVbot2be3DY1I_H2C_xvlcSHSFKWVwsXr7DJyMyU7Wrsr_B2AAHsr0TY_5RFZ46agwjmiNSXO3s6VeoC2pbHd-pWSEen6LyXo9T_QAlY7YKrdCw0jWV9r7UGVmmuZsYoezdvay4FInugAYKI4c0mqldonnYekpXyGtfHoTXG719SlK75W9mB4Fl3F1sJb4HDseDUOlbI9rYuH0l5LvFCHc2nGL_uaRWK8kPMAo-1OdoxY2uOMVeW6xfY6TsrNgAuUlJ6H3-SDZuo-ItH1m12j8qbKClrlPVPEIq8Z6HiB5PQc3KsSv9Cvms7aO-1aLEKTBJGBtd9lCgVgPdlhnTHwt7okv-HcU1vua6qCcIaAviuYKXTnAphqHXhkpuj4avV7cmaM_ZKJEABx8_vKLTc5cmhFEVdQEnNagyx64u9UMnHM-BJBUsw8Q092QiuO-Vd9_GYFTjO8p_R3UVNzUSzfTHSHwSXbKZhT_R0u8Uz4PfyCubLGzsLAWslC_2PKCCw_Wn-fBljzU7OFX_W-q-A2FAEx_LYJXa4hH0U7tFvDi7EJkMqli-NMo4kOW2UzQzTttStzNFUsUI7sUCq7Tg51Tevx26hdy2l_3KNO06FuUJB5Cq0inUiAEl2nNSJCW5E4mLkUSWPsQcDPKWfnFvMSHpLtqweROXnm4vVt6PWnjx67mT54Js-mcchl9sBo4m2nGpBSjkgPGyJ5m8GdrG--hqxyvAHXjhzdzTF3-wjXQsU1MJB8g7f1DVFFpiJ-xMWaeCJ7nbrFFTqvMkUagM83jnsTunU7hsguMcjmkYK4mMIqJ-A9yGNAkvW5j4SLIM6L_XzT5PNIlEoiyNp-wimccncK1z48o6ClAgy9X7c3B7el5eq2_89TWMiu7jOtZTuPLhhZy6k2X89fNf6EiggeJ_ww-6i_Wi12UhL-SgShETVypb4Iqghx5HfDpKlj9cgVRj_adtjM5TLHOuNoRMsMjyao2MmyXWQHSjrMZaru7SlJtbro3ojXFSeQn8Ftf1SNRv5vymSoo3iNMh6bzDxlvHlxqo6qUtCjQGzGpWgmqyD_mHF17oPCnzyoBxaFmxKWLT3bKlkDSVx6802YCw-x4yisXqtuWxH-tOURENzaCXJlf0-ceXgqvicbYVqnfECA9i48fiDFGtHdyurFL3Ck9IMrTr7FBNiTOfsiqEzrgqLMKYd3fB9pBhunr6HMQ0BcMCXcw-bMwpKMTS4vZBS4sQ5dn2Ww8554RhGW6EPO7cF0XxsltEGUjlxgEzuzs-Fs3dbW0MKVebBq5ALS8VqNCZXWA8nUlbTtbgvWJuJLHo_2QRi8tsaRWgelH_bb9ZaBW2dfYdhdfiFKpk_jTbQS-hti2-qWVjK7Zg0p08Bw"
+     *     responses:
+     *       '100':
+     *         description: User logged in successfully.
+     */
     app.post(
         LoginRoute,
         client_log_message(LoginRoute, async (req, _res, client) => {
@@ -97,6 +128,34 @@ const token = (app: Express) => {
             });
         })
     );
+
+    /**
+     * @swagger
+     * /logout:
+     *   post:
+     *     summary: Logout with provided data.
+     *     parameters:
+     *         - in: header
+     *           name: secret
+     *           required: true
+     *           schema:
+     *             type: string
+     *           description: API key or token for authentication
+     *     description: Logout with provided data.
+     *     tags:
+     *      - Token
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *             example:
+     *     responses:
+     *       '100':
+     *         description: User logged in successfully.
+     */
     app.post(
         LogoutRoute,
         client_log_message(LogoutRoute, async (req, _res, client) => {
